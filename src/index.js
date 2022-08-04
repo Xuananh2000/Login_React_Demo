d85f4428c31b3230afe4f166137f5b06a283e917
XA_140920 <103247610+Xuananh2000@users.noreply.github.com>
2022-08-04 16:00:55 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Logigitn from './components/pages/Login';
+import Login from './components/pages/Login';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap-theme.css';
